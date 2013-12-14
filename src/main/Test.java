2738c5b7dd3cce7e29120830dc1649248266e2e8
Zephyr <zephyrsd@hotmail.fr>
2013-12-14 17:25:41 +0100

    add IHM package

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,20 +1,22 @@
 package main;
 
+import javax.swing.SwingUtilities;
+
+import IHM.Fenetre;
+
 import objets.Animal;
 import objets.Objet;
 import map.Map;
 
 public class Test {
 	public static void main(String[] args) {
-		Map map = new Map(50,50);
-		
-		map.ajouterObjet(new Objet(3,2));
-		map.ajouterObjet(new Objet(4,2));
-		map.ajouterObjet(new Objet(3,2));
-		
-		Animal a = new Animal(4,2,1);
-		map.ajouterObjet(a);
-		System.out.println(map.getVision(a));
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				//On crée une nouvelle instance de notre JDialog
+				Fenetre fenetre = new Fenetre();
+				fenetre.setVisible(true);//On la rend visible
+			}
+		});
 		
 		
 	}
